20fa6ce78a0d83c32162975f31a4423a987bc014
varshachougule22 <90234080+varshachougule22@users.noreply.github.com>
2021-09-12 08:55:27 +0530

    Add files via upload

--- missing_number.c
+++ missing_number.c
@@ -1,1 +1,20 @@
 
+#include <stdio.h>
+
+int nu(int a[], int n)
+{
+    int i, total;
+    total = (n + 1) * (n + 2) / 2;
+    for (i = 0; i < n; i++)
+        total -= a[i];
+    return total;
+}
+
+
+int main()
+{
+    int a[] = {7,3,8,4,5,1,6};
+    int find = nu(a, 7);
+    printf("%d", find);
+    getchar();
+}
